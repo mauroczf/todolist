e2f14dfac323f2b1d2c413fa5711d867d9e129a2
mauroczf <mauroczf@gmail.com>
2022-06-22 12:16:59 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Loading from "./components/Loading";
+import TodoList from "./components/TodoList";
+
+import axios from "axios";
+
+function App() {
+  const [todos, setTodos] = useState(null);
+
+  const onUpdateTodo = (todo) => {
+    const todoItemIndex = todos.findIndex((x) => x.id == todo.id);
+    const newTodos = [...todos];
+
+    const newTodo = newTodos[todoItemIndex];
+    newTodo.completed = !newTodo.completed;
+    newTodos[todoItemIndex] = newTodo;
+    setTodos(newTodos);
+  };
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((result) => {
+      setTodos(result.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {todos ? (
+        <TodoList todos={todos} onUpdateTodo={onUpdateTodo} />
+      ) : (
+        <Loading />
+      )}
+    </div>
+  );
+}
+
+export default App;
